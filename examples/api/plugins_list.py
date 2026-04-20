b237ee06788f6b19006d19c5ab16c48253ffa6a3
Danny Willems <be.danny.willems@gmail.com>
2026-04-20 10:53:17 -0300

    Fix Python examples to match client API signatures

--- examples/api/plugins_list.py
+++ examples/api/plugins_list.py
@@ -3,4 +3,4 @@ from leakix import Client
 client = Client(api_key="YOUR_API_KEY")
 response = client.get_plugins()
 for plugin in response.json():
-    print(plugin)
+    print(plugin.name, plugin.description)
